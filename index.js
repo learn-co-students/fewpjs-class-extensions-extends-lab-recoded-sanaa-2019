ac650e894770cf2e221e04e648e5e87db87387b9
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-18 12:15:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
 // Your code here
+
+class Polygon {
+  constructor(array){
+    this.array = array 
+
+  }
+  
+  get countSides(){
+    return this.array.length
+  }
+  
+  get perimeter(){
+    let sum = 0;
+    this.array.forEach(item =>{
+      sum+=item;
+    })
+    return sum
+  }
+}
+
+
+class Triangle extends Polygon{
+  
+  constructor(array){
+    super(array)
+  }
+  
+            get isValid() {
+                if (this.array[0] + this.array[1] > this.array[2] && this.array[0] + this.array[2] > this.array[1] && this.array[2] + this.array[1] > this.array[0]) {
+                    console.log(true);
+                    return true
+                } else {
+                    console.log(false);
+                    return false
+
+                }
+            }
+  
+}
+
+
+
+class Square extends Polygon{
+  constructor(array){
+    super(array)
+  }
+  
+  get area(){
+    let ar = 0
+    
+    ar = this.array[0] * this.array[0]
+
+    return ar
+  }
+  
+  get isValid(){
+
+let check =true;
+        for (let index = 0; index < this.array.length; index++) {
+            const element = this.array[index];
+            
+            if (this.array[0] !== element) {
+                //console.log(false);
+                check =false;
+            }
+            
+        }
+        return check;
+}
+}
