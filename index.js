5199d8adaa491eec6f77dd6bd475e0050028758d
Sarah Alafif <sar1997yah@gmail.com>
2019-12-06 20:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,36 +17,27 @@ class Triangle extends Polygon{
 constructor(array){
     super(array)
   }
-get isValid() {if (this.array[0] + this.array[1] > this.array[2] && this.array[0] + this.array[2] > this.array[1] && this.array[2] + this.array[1] > this.array[0]) {return true
-                } else {
-                    console.log(false);
-                    return false
-              }
-            }
+get isValid() {if (this.array[0] + this.array[1] > this.array[2] && this.array[0] + this.array[2] > this.array[1] && this.array[2] + this.array[1] > this.array[0]) {return true } else {
+              return false } }
 }
-
-
 
 class Square extends Polygon{
  constructor(array){
    super(array)
   }
 get area(){
-    let ar = 0
-  ar = this.array[0] * this.array[0]
+let ar = 0
+ar = this.array[0] * this.array[0]
 return ar
   }
   get isValid(){
 let check =true;
-        for (let index = 0; index < this.array.length; index++) {
-            const element = this.array[index];
-
-            if (this.array[0] !== element) {
-      
-                check =false;
-            }
-
-        }
-        return check;
+for (let index = 0; index < this.array.length; index++) {
+  const element = this.array[index];
+if (this.array[0] !== element) {
+ check =false;
+ }
+ }
+ return check;
 }
 }
