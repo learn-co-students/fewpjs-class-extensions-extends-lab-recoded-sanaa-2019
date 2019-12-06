54de01851847efa1b0328d9e991c3367bce48e57
Sarah Alafif <sar1997yah@gmail.com>
2019-12-06 20:11:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 class Polygon {
   constructor(array){
     this.array = array 
-
-  }
-
-  get countSides(){
+}
+get countSides(){
     return this.array.length
   }
-
-  get perimeter(){
+get perimeter(){
     let sum = 0;
     this.array.forEach(item =>{
       sum+=item;
@@ -16,39 +13,30 @@ class Polygon {
     return sum
   }
 }
-
-
 class Triangle extends Polygon{
-
-  constructor(array){
+constructor(array){
     super(array)
   }
-
-            get isValid() {
+get isValid() {
                 if (this.array[0] + this.array[1] > this.array[2] && this.array[0] + this.array[2] > this.array[1] && this.array[2] + this.array[1] > this.array[0]) {
                     console.log(true);
                     return true
                 } else {
                     console.log(false);
                     return false
-
-                }
+              }
             }
-
 }
 
 
 
 class Square extends Polygon{
-  constructor(array){
-    super(array)
+ constructor(array){
+   super(array)
   }
-
-  get area(){
+get area(){
     let ar = 0
-
-    ar = this.array[0] * this.array[0]
-
+  ar = this.array[0] * this.array[0]
 return ar
   }
   get isValid(){
